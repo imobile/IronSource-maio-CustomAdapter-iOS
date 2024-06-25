f732db8b9dfab0187ee7c61cc9237a4721b9d638
im-ttmskk <tatsu-kasamatsu@i-mobile.co.jp>
2024-06-25 22:57:56 +0900

    Make Framework project

---
+++ ISMaioCustomAdapter/ISMaioCustomAdapter/ISMaioCustomAdapter.h
@@ -0,0 +1,16 @@
+//
+//  ISMaioCustomAdapter.h
+//  ISMaioCustomAdapter
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ISMaioCustomAdapter.
+FOUNDATION_EXPORT double ISMaioCustomAdapterVersionNumber;
+
+//! Project version string for ISMaioCustomAdapter.
+FOUNDATION_EXPORT const unsigned char ISMaioCustomAdapterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ISMaioCustomAdapter/PublicHeader.h>
+
+
